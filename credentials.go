0e34b6c4551fba1eceab3125a5956bd02b296100
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-09-09 14:23:01 -0500

    Download make-coe credentials
    This required updating the bundle format because different coe's need
    different scripts, e.g. docker.env vs kubectl.config

---
+++ credentials.go
@@ -0,0 +1,150 @@
+package libcarina
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/url"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+const verifyCredentialsTimeout = 2 * time.Second
+
+// CredentialsBundle is a set of certificates and environment information necessary to connect to a cluster
+type CredentialsBundle struct {
+	Files map[string][]byte
+	Err   error
+}
+
+// NewCredentialsBundle initializes an empty credentials bundle
+func NewCredentialsBundle() *CredentialsBundle {
+	return &CredentialsBundle{
+		Files: make(map[string][]byte),
+	}
+}
+
+// LoadCredentialsBundle loads a credentials bundle from the filesystem
+func LoadCredentialsBundle(credentialsPath string) CredentialsBundle {
+	var creds CredentialsBundle
+
+	files, err := ioutil.ReadDir(credentialsPath)
+	if err != nil {
+		creds.Err = errors.Wrapf(err, "Invalid credentials bundle. Cannot list files in %s", credentialsPath)
+		return creds
+	}
+
+	creds.Files = make(map[string][]byte)
+	for _, file := range files {
+		filePath := filepath.Join(credentialsPath, file.Name())
+		fileContents, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			creds.Err = errors.Wrapf(err, "Invalid credentials bundle. Cannot read %s", filePath)
+			return creds
+		}
+		creds.Files[file.Name()] = fileContents
+	}
+
+	return creds
+}
+
+// GetCA returns the contents of ca.pem
+func (creds CredentialsBundle) GetCA() []byte {
+	return creds.Files["ca.pem"]
+}
+
+// GetCert returns the contents of cert.pem
+func (creds CredentialsBundle) GetCert() []byte {
+	return creds.Files["cert.pem"]
+}
+
+// GetKey returns the contents of key.pem
+func (creds CredentialsBundle) GetKey() []byte {
+	return creds.Files["key.pem"]
+}
+
+// Verify validates that we can connect to the Docker host specified in the credentials bundle
+func (creds CredentialsBundle) Verify() error {
+	if creds.Err != nil {
+		return creds.Err
+	}
+
+	tlsConfig, err := creds.GetTLSConfig()
+	if err != nil {
+		return err
+	}
+
+	host, err := creds.ParseHost()
+	if err != nil {
+		return err
+	}
+
+	telephone := &net.Dialer{Timeout: verifyCredentialsTimeout}
+	conn, err := tls.DialWithDialer(telephone, "tcp", host, tlsConfig)
+	if err != nil {
+		return errors.Wrapf(err, "Invalid credentials bundle. Unable to connect to %s.", host)
+	}
+	conn.Close()
+
+	return nil
+}
+
+// ParseHost finds the COE Endpoint, e.g. the swarm or kubernetes ip and port
+func (creds CredentialsBundle) ParseHost() (string, error) {
+	var host string
+	var ok bool
+
+	if config, isDocker := creds.Files["docker.env"]; isDocker {
+		host, ok = parseHost(config, "DOCKER_HOST=")
+		if !ok {
+			return "", errors.New("Invalid credentials bundle. Could not parse DOCKER_HOST from docker.env.")
+		}
+	} else if config, isKubernetes := creds.Files["kubectl.config"]; isKubernetes {
+		host, ok = parseHost(config, "server:")
+		if !ok {
+			return "", errors.New("Invalid credentials bundle. Could not parse server from kubectl.config.")
+		}
+	} else {
+		return "", errors.New("Invalid credentials bundle. Missing both docker.env and kubectl.config.")
+	}
+
+	hostURL, err := url.Parse(host)
+	if err != nil {
+		return "", fmt.Errorf("Invalid credentials bundle. Bad host URL %s", host)
+	}
+
+	return hostURL.Host, nil
+}
+
+func parseHost(config []byte, token string) (string, bool) {
+	lines := strings.Split(string(config), "\n")
+	for _, line := range lines {
+		host := strings.Split(line, token)
+		if len(host) == 2 {
+			return strings.TrimSpace(host[1]), true
+		}
+	}
+
+	return "", false
+}
+
+// GetTLSConfig puts together the necessary TLS configuration to connect to the COE Endpoint returned by ParseHost
+func (creds CredentialsBundle) GetTLSConfig() (*tls.Config, error) {
+	var tlsConfig tls.Config
+	tlsConfig.InsecureSkipVerify = true
+	certPool := x509.NewCertPool()
+
+	certPool.AppendCertsFromPEM(creds.GetCA())
+	tlsConfig.RootCAs = certPool
+	keypair, err := tls.X509KeyPair(creds.GetCert(), creds.GetKey())
+	if err != nil {
+		return &tlsConfig, errors.Wrap(err, "Invalid credentials bundle. Keypair mis-match.")
+	}
+	tlsConfig.Certificates = []tls.Certificate{keypair}
+	return &tlsConfig, nil
+}
--- libcarina.go
+++ libcarina.go
@@ -3,8 +3,6 @@ package libcarina
 import (
 	"archive/zip"
 	"bytes"
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -22,7 +20,6 @@ import (
 // BetaEndpoint reflects the default endpoint for this library
 const BetaEndpoint = "https://app.getcarina.com"
 const mimetypeJSON = "application/json"
-const authHeaderKey = "X-Auth-Token"
 const userAgent = "getcarina/libcarina"
 
 // ZipURLResponse is the response that comes back from the zip endpoint
@@ -72,21 +69,6 @@ type Cluster struct {
 	Status string `json:"status,omitempty"`
 }
 
-// Credentials holds the keys to the kingdom
-type Credentials struct {
-	README     []byte
-	Cert       []byte
-	Key        []byte
-	CA         []byte
-	CAKey      []byte
-	DockerEnv  []byte
-	DockerCmd  []byte
-	DockerPS1  []byte
-	DockerHost string
-	Files      map[string][]byte
-	DockerFish []byte
-}
-
 // Quotas is the set of account quotas
 type Quotas struct {
 	MaxClusters        int `json:"max_clusters"`
@@ -124,10 +106,12 @@ func (c *ClusterClient) NewRequest(method string, uri string, body io.Reader) (*
 	if err != nil {
 		return nil, err
 	}
+
 	req.Header.Set("User-Agent", userAgent)
 	req.Header.Add("Content-Type", mimetypeJSON)
 	req.Header.Add("Accept", mimetypeJSON)
-	req.Header.Add(authHeaderKey, c.Token)
+	req.Header.Add("X-Auth-Token", c.Token)
+
 	resp, err := c.Client.Do(req)
 	if err != nil {
 		return nil, err
@@ -241,38 +225,21 @@ func (c *ClusterClient) Create(clusterOpts Cluster) (*Cluster, error) {
 	return clusterFromResponse(resp, err)
 }
 
-// GetZipURL returns the URL for downloading credentials
-func (c *ClusterClient) GetZipURL(clusterName string) (string, error) {
-	uri := path.Join("/clusters", c.Username, clusterName, "zip")
-	resp, err := c.NewRequest("GET", uri, nil)
-	if err != nil {
-		return "", err
-	}
-
-	var zipURLResp ZipURLResponse
-	defer resp.Body.Close()
-	err = json.NewDecoder(resp.Body).Decode(&zipURLResp)
-	if err != nil {
-		return "", err
-	}
-
-	return zipURLResp.URL, nil
-}
-
 // GetCredentials returns a Credentials struct for the given cluster name
-func (c *ClusterClient) GetCredentials(clusterName string) (*Credentials, error) {
-	url, err := c.GetZipURL(clusterName)
+func (c *ClusterClient) GetCredentials(token string) (*CredentialsBundle, error) {
+	id, err := c.lookupClusterID(token)
 	if err != nil {
 		return nil, err
 	}
+
+	url := c.Endpoint + path.Join("/clusters", id, "credentials/zip")
 	zr, err := fetchZip(c.Client, url)
-	if err != nil || len(zr.File) < 6 {
+	if err != nil {
 		return nil, err
 	}
 
-	// fetch the contents for each credential/note
-	creds := new(Credentials)
-	creds.Files = make(map[string][]byte)
+	// fetch the contents for each file
+	creds := NewCredentialsBundle()
 	for _, zf := range zr.File {
 		_, fname := path.Split(zf.Name)
 		fi := zf.FileInfo()
@@ -292,76 +259,9 @@ func (c *ClusterClient) GetCredentials(clusterName string) (*Credentials, error)
 			return nil, err
 		}
 		creds.Files[fname] = b
-
-		switch fname {
-		case "ca.pem":
-			creds.CA = b
-		case "README.md":
-			creds.README = b
-		case "ca-key.pem":
-			creds.CAKey = b
-		case "docker.env":
-			creds.DockerEnv = b
-		case "cert.pem":
-			creds.Cert = b
-		case "key.pem":
-			creds.Key = b
-		case "docker.ps1":
-			creds.DockerPS1 = b
-		case "docker.cmd":
-			creds.DockerCmd = b
-		case "docker.fish":
-			creds.DockerFish = b
-		}
-
-	}
-
-	sourceLines := strings.Split(string(creds.DockerEnv), "\n")
-	for _, line := range sourceLines {
-		if strings.Index(line, "export ") == 0 {
-			varDecl := strings.TrimRight(line[7:], "\n")
-			eqLocation := strings.Index(varDecl, "=")
-
-			varName := varDecl[:eqLocation]
-			varValue := varDecl[eqLocation+1:]
-
-			switch varName {
-			case "DOCKER_HOST":
-				creds.DockerHost = varValue
-			}
-
-		}
 	}
 
 	return creds, nil
-}
-
-// GetDockerConfig returns the hostname and tls.Config for a given clustername
-func (c *ClusterClient) GetDockerConfig(clusterName string) (hostname string, tlsConfig *tls.Config, err error) {
-	creds, err := c.GetCredentials(clusterName)
-	if err != nil {
-		return "", nil, err
-	}
-	tlsConfig, err = creds.GetTLSConfig()
-	return creds.DockerHost, tlsConfig, err
-}
-
-// GetTLSConfig returns a tls.Config for a credential set
-func (creds *Credentials) GetTLSConfig() (*tls.Config, error) {
-	// TLS config
-	var tlsConfig tls.Config
-	tlsConfig.InsecureSkipVerify = true
-	certPool := x509.NewCertPool()
-
-	certPool.AppendCertsFromPEM(creds.CA)
-	tlsConfig.RootCAs = certPool
-	keypair, err := tls.X509KeyPair(creds.Cert, creds.Key)
-	if err != nil {
-		return &tlsConfig, err
-	}
-	tlsConfig.Certificates = []tls.Certificate{keypair}
-
-	return &tlsConfig, nil
 }
 
 func fetchZip(client *http.Client, zipurl string) (*zip.Reader, error) {
